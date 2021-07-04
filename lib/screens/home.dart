c12191e194af7be90c9cf47243b8e85ab1f7caf2
vashishtha <vashishthasiyaram@gmail.com>
2021-07-04 22:00:19 +0530

    Linear Progress

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -24,9 +24,12 @@ class HomeScreen extends StatelessWidget {
                   Divider(),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      "Skills",
-                      style: Theme.of(context).textTheme.headline5,
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "Skills",
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
                     ),
                   ),
                   Row(
@@ -43,20 +46,78 @@ class HomeScreen extends StatelessWidget {
                     ],
                   ),
                   Divider(),
-                  // SizedBox(height: defaultPadding),
-                  // Padding(
-                  //   padding: const EdgeInsets.all(8.0),
-                  //   child: Text(
-                  //     "Coding",
-                  //     style: Theme.of(context).textTheme.headline5,
-                  //   ),
-                  // ),
+                  SizedBox(height: defaultPadding),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "Coding",
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        LinearProgress(text: "Dart",percentage: 0.8),
+                        LinearProgress(text: "Dart",percentage: 0.8),
+                        LinearProgress(text: "Dart",percentage: 0.8),
+                        LinearProgress(text: "Dart",percentage: 0.8),
+                        LinearProgress(text: "Dart",percentage: 0.8)
+                      ],
+                    ),
+                  ),
+                  Divider()
                 ],
               ),
             ),
           ))
         ]),
       )),
+    );
+  }
+}
+
+class LinearProgress extends StatelessWidget {
+  const LinearProgress({
+    Key? key,
+    required this.text,
+    required this.percentage,
+  }) : super(key: key);
+  final String text;
+  final double percentage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TweenAnimationBuilder(
+        tween: Tween<double>(begin: 0, end: 0.75),
+        duration: defaultDuration,
+        builder: (context, double value, child) => Column(
+          children: [
+            Row(
+              mainAxisAlignment:
+                  MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Dart",
+                    style: Theme.of(context)
+                        .textTheme
+                        .subtitle2),
+                Text((value * 100).toInt().toString() + "%")
+              ],
+            ),
+            SizedBox(height: defaultPadding / 2),
+            LinearProgressIndicator(
+              value: value,
+              color: primaryColor,
+              backgroundColor: darkColor,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -73,34 +134,38 @@ class CircularProgress extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AspectRatio(
-          aspectRatio: 1,
-          child: TweenAnimationBuilder(
-            tween: Tween<double>(begin: 0, end: percentage),
-            duration: defaultDuration,
-            builder: (context, double value, child) => Stack(
-              fit: StackFit.expand,
-              children: [
-                CircularProgressIndicator(
-                  value: value,
-                  color: primaryColor,
-                ),
-                Center(
-                  child: Text((value * 100).toInt().toString() + "%",
-                      style: Theme.of(context).textTheme.subtitle1),
-                )
-              ],
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: 1,
+            child: TweenAnimationBuilder(
+              tween: Tween<double>(begin: 0, end: percentage),
+              duration: defaultDuration,
+              builder: (context, double value, child) => Stack(
+                fit: StackFit.expand,
+                children: [
+                  CircularProgressIndicator(
+                    value: value,
+                    color: primaryColor,
+                    backgroundColor: darkColor,
+                  ),
+                  Center(
+                    child: Text((value * 100).toInt().toString() + "%",
+                        style: Theme.of(context).textTheme.subtitle1),
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-        SizedBox(height: defaultPadding / 2),
-        Text(
-          text,
-          style: Theme.of(context).textTheme.subtitle2,
-        ),
-      ],
+          SizedBox(height: defaultPadding / 2),
+          Text(
+            text,
+            style: Theme.of(context).textTheme.subtitle2,
+          ),
+        ],
+      ),
     );
   }
 }
